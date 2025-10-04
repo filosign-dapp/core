f422bcd322390a19a119a4fb95376818ada1f088
Spandan Barve <spandan567@gmail.com>
2025-10-05 04:58:33 +0800

    add user routes to main router

--- packages/server/api/routes/router.ts
+++ packages/server/api/routes/router.ts
@@ -2,8 +2,10 @@ import { Hono } from "hono";
 import auth from "./auth";
 import files from "./files";
 import requests from "./requests";
+import user from "./user";
 
 export const apiRouter = new Hono()
   .route("/auth", auth)
   .route("/files", files)
-  .route("/requests", requests);
+  .route("/requests", requests)
+  .route("/user", user);
