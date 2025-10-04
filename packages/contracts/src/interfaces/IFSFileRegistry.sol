cc2a2571d00673a5a6b6433c8a119a19de32db9c
Spandan Barve <spandan567@gmail.com>
2025-10-05 05:16:17 +0800

    new interfaces

--- packages/contracts/src/interfaces/IFSFileRegistry.sol
+++ packages/contracts/src/interfaces/IFSFileRegistry.sol
@@ -7,7 +7,7 @@ interface IFSFileRegistry {
     struct FileData {
         bytes32 pieceCidPrefix;
         address sender;
-        address[] recipients;
+        mapping(address => bool) recipients;
         uint16 pieceCidTail;
         bool acked;
     }
@@ -23,14 +23,22 @@ interface IFSFileRegistry {
         bytes32 s;
     }
 
+    struct FileDataView {
+        bytes32 pieceCidPrefix;
+        address sender;
+        uint16 pieceCidTail;
+        bool acked;
+    }
+
     function manager() external view returns (address);
     event FileRegistered();
     event FileAcknowledged();
     event SignatureSubmitted();
     function acknowledge(bytes32 cidIdentifier_) external;
-    function registerFile(bytes32 pieceCidPrefix_, uint16 pieceCidTail_, address[] recipients_) external;
+    function registerFile(bytes32 pieceCidPrefix_, uint16 pieceCidTail_, address[] calldata recipients_) external;
     function submitSignature(bytes32 cidIdentifier_, bytes32 signatureVisualHash_, uint8 v_, bytes32 r_, bytes32 s_) external;
     function cidIdentifier(bytes32 pieceCidPrefix_, uint16 pieceCidTail_) external pure returns (bytes32);
-    function getFileData(bytes32 cidIdentifier_) external view returns (FileData memory);
+    function getFileData(bytes32 cidIdentifier_) external view returns (FileDataView memory);
+    function isRecipient(bytes32 cidIdentifier_, address recipient_) external view returns (bool);
     function getSignatureData(bytes32 cidIdentifier_) external view returns (SignatureData memory);
 }
