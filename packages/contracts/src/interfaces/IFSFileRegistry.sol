f94b0445f64add879ced553ab4c8892c502c1e81
Spandan Barve <spandan567@gmail.com>
2025-10-07 04:35:39 +0800

    update interfaces

--- packages/contracts/src/interfaces/IFSFileRegistry.sol
+++ packages/contracts/src/interfaces/IFSFileRegistry.sol
@@ -6,10 +6,9 @@ pragma solidity ^0.8.26;
 interface IFSFileRegistry {
     struct FileData {
         bytes32 pieceCidPrefix;
+        bytes16 pieceCidBuffer;
+        bytes32 pieceCidTail;
         address sender;
-        uint16 pieceCidTail;
-        bool pieceCidParity;
-        uint8 missingByte;
         mapping(address => bool) recipients;
         mapping(address => bool) acked;
     }
@@ -27,10 +26,9 @@ interface IFSFileRegistry {
 
     struct FileDataView {
         bytes32 pieceCidPrefix;
+        bytes16 pieceCidBuffer;
+        bytes32 pieceCidTail;
         address sender;
-        uint16 pieceCidTail;
-        bool pieceCidParity;
-        uint8 missingByte;
     }
 
     function manager() external view returns (address);
@@ -38,9 +36,9 @@ interface IFSFileRegistry {
     event FileAcknowledged();
     event SignatureSubmitted();
     function acknowledge(bytes32 cidIdentifier_) external;
-    function registerFile(bytes32 pieceCidPrefix_, uint16 pieceCidTail_, bool pieceCidParity_, uint8 missingByte_, address[] calldata recipients_) external;
+    function registerFile(bytes32 pieceCidPrefix_, bytes16 pieceCidBuffer_, bytes32 pieceCidTail_, address[] calldata recipients_) external;
     function submitSignature(bytes32 cidIdentifier_, bytes32 signatureVisualHash_, uint8 v_, bytes32 r_, bytes32 s_) external;
-    function cidIdentifier(bytes32 pieceCidPrefix_, uint16 pieceCidTail_) external pure returns (bytes32);
+    function cidIdentifier(bytes32 pieceCidPrefix_, bytes16 pieceCidBuffer_, bytes32 pieceCidTail_) external pure returns (bytes32);
     function getFileData(bytes32 cidIdentifier_) external view returns (FileDataView memory);
     function isRecipient(bytes32 cidIdentifier_, address recipient_) external view returns (bool);
     function isAcknowledged(bytes32 cidIdentifier_, address recipient_) external view returns (bool);
