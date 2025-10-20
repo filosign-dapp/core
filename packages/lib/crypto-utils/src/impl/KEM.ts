d5359e44289338b0aeaace4e3485c2fd875fc089
Spandan Barve <spandan567@gmail.com>
2025-10-20 21:59:18 +0530

    better KEM api exposd

--- packages/lib/crypto-utils/src/impl/KEM.ts
+++ packages/lib/crypto-utils/src/impl/KEM.ts
@@ -1,29 +1,11 @@
 import kyber from "crystals-kyber";
 
-type KeyPair = {
-	publicKey: Uint8Array;
-	secretKey: Uint8Array;
-};
-
-type EncapResult = {
-	ciphertext: Uint8Array;
-	sharedSecret: Uint8Array;
-};
-
-type DecapResult = {
-	sharedSecret: Uint8Array;
-};
-
-type ComputeSharedArgs =
-	| { publicKeyOther: Uint8Array } // initiator: encapsulate to peer's public key
-	| { ciphertext: Uint8Array; secretKeySelf: Uint8Array }; // responder: decapsulate
-
-function toUint8(a: ArrayBuffer | Uint8Array | Buffer): Uint8Array {
-	if (a instanceof Uint8Array) return a;
-	return new Uint8Array(a);
+function toUint8(input: ArrayBuffer | Uint8Array | Buffer) {
+	if (input instanceof Uint8Array) return input;
+	return new Uint8Array(input);
 }
 
-export function keyGen(): KeyPair {
+export function keyGen() {
 	const pair = kyber.KeyGen1024();
 	return {
 		publicKey: toUint8(pair[0]),
@@ -31,18 +13,15 @@ export function keyGen(): KeyPair {
 	};
 }
 
-export function computeShared(
-	args: ComputeSharedArgs,
-): EncapResult | DecapResult {
-	if ("publicKeyOther" in args) {
-		const [ct, ss] = kyber.Encrypt1024(args.publicKeyOther);
-		return { ciphertext: toUint8(ct), sharedSecret: toUint8(ss) };
-	}
+export function encapsulate(publicKeyOther: Uint8Array) {
+	const [ciphertext, sharedSecret] = kyber.Encrypt1024(publicKeyOther);
+	return {
+		ciphertext: toUint8(ciphertext),
+		sharedSecret: toUint8(sharedSecret),
+	};
+}
 
-	if ("ciphertext" in args && "secretKeySelf" in args) {
-		const ss = kyber.Decrypt1024(args.ciphertext, args.secretKeySelf);
-		return { sharedSecret: toUint8(ss) };
-	}
-
-	throw new Error("Invalid arguments to computeShared");
+export function decapsulate(ciphertext: Uint8Array, secretKeySelf: Uint8Array) {
+	const sharedSecret = kyber.Decrypt1024(ciphertext, secretKeySelf);
+	return { sharedSecret: toUint8(sharedSecret) };
 }
