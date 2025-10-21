b9862c74668e0047c291e0f62a0643f73fbecc83
Spandan Barve <spandan567@gmail.com>
2025-10-21 23:04:15 +0530

    going down the EIP712 rote for meta txns in keyreg

--- packages/contracts/src/FSKeyRegistry.sol
+++ packages/contracts/src/FSKeyRegistry.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.26;
 
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+
 import "./interfaces/IFSManager.sol";
 
-contract FSKeyRegistry {
+contract FSKeyRegistry is EIP712 {
     struct KeygenData {
         bytes16 salt_pin;
         bytes16 salt_seed;
@@ -16,7 +18,12 @@ contract FSKeyRegistry {
 
     event KeygenDataRegistered(address indexed user);
 
-    constructor() {}
+    constructor() EIP712("Filosign File Registry", "1") {}
+
+    bytes32 private constant REGISTERKEYGENDATA_TYPEHASH =
+        keccak256(
+            "Signature(bytes16 salt_pin,bytes16 salt_seed,bytes20 commitment_kyber_pk,bytes20 commitment_dilithium_pk)"
+        );
 
     function isRegistered(address user_) public view returns (bool) {
         return
@@ -25,19 +32,20 @@ contract FSKeyRegistry {
     }
 
     function registerKeygenData(
-        bytes16 salt_pin,
-        bytes16 salt_seed,
-        bytes20 commitment_kyber_pk,
-        bytes20 commitment_dilithium_pk
+        bytes16 salt_pin_,
+        bytes16 salt_seed_,
+        bytes20 commitment_kyber_pk_,
+        bytes20 commitment_dilithium_pk_,
+        bytes32 signature_
     ) external {
-        require(salt_pin != bytes16(0), "Invalid salt_pin");
-        require(salt_seed != bytes16(0), "Invalid salt_seed");
+        require(salt_pin_ != bytes16(0), "Invalid salt_pin");
+        require(salt_seed_ != bytes16(0), "Invalid salt_seed");
         require(
-            commitment_kyber_pk != bytes20(0),
+            commitment_kyber_pk_ != bytes20(0),
             "Invalid commitment_kyber_pk"
         );
         require(
-            commitment_dilithium_pk != bytes20(0),
+            commitment_dilithium_pk_ != bytes20(0),
             "Invalid commitment_dilithium_pk"
         );
         require(isRegistered(msg.sender) == false, "Data already registered");
