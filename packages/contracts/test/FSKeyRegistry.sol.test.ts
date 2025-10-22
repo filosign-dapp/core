7a23b23a8314aee82f2879baf93e8052e400219c
Spandan Barve <spandan567@gmail.com>
2025-10-23 04:50:34 +0530

    remove manual kegen from testing of contracts

--- packages/contracts/test/FSKeyRegistry.sol.test.ts
+++ packages/contracts/test/FSKeyRegistry.sol.test.ts
@@ -2,14 +2,19 @@ import {
 	computeCommitment,
 	hash as fsHash,
 	generateRegisterChallenge,
+	hkdfExtractExpand,
 	KEM,
 	randomBytes,
+	signatures,
+	toBytes,
+	toHex,
 } from "@filosign/crypto-utils/node";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 import { describe, it } from "mocha";
 import {
+	concatBytes,
 	concatHex,
 	encodePacked,
 	keccak256,
@@ -35,79 +40,7 @@ describe("FSKeyRegistry", () => {
 	it("stores relevant information for the user to be able to regenerate encryption keys", async () => {
 		const { keyRegistry, user, admin } = await loadFixture(setupFixture);
 		const pin = "1234";
-		const pinArgoned = fsHash.argon(fsHash.hash(pin));
-		const pinSalt = randomBytes(16);
 
-		const commitment_pin = computeCommitment([pin, pinSalt.toString()]);
-
-		const challengeSalt = randomBytes(16);
-		const registerChallenge = generateRegisterChallenge(
-			user.account.address,
-			toHex(challengeSalt),
-			pinArgoned,
-		);
-
-		const encSeedHex = `0x${toHex(enc_material.encSeed)}` as const;
-
-		const txHash = await keyRegistry.write.registerKeygenData(
-			[
-				{
-					nonce: `0x${toHex(nonce)}`,
-					salt_pin: `0x${toHex(base_material.pinSalt)}`,
-					salt_auth: `0x${toHex(base_material.authSalt)}`,
-					salt_wrap: `0x${toHex(base_material.wrapperSalt)}`,
-					seed_head: sliceHex(encSeedHex, 0, 20),
-					seed_word: sliceHex(encSeedHex, 20, 52),
-					seed_tail: sliceHex(encSeedHex, 52, 72),
-					commitment_pin,
-				},
-				`0x${toHex(publicKey)}`,
-			],
-			{ account: user.account },
-		);
-
-		await admin.waitForTransactionReceipt({ hash: txHash });
-
-		const [
-			stored_salt_auth,
-			stored_salt_wrap,
-			stored_salt_pin,
-			stored_nonce,
-			stored_seed_head,
-			stored_seed_word,
-			stored_seed_tail,
-		] = await keyRegistry.read.keygenData([user.account.address]);
-
-		const stored = {
-			salt_auth: toB64(stored_salt_auth),
-			salt_wrap: toB64(stored_salt_wrap),
-			salt_pin: toB64(stored_salt_pin),
-			nonce: toB64(stored_nonce),
-			seed: toB64(
-				concatHex([stored_seed_head, stored_seed_word, stored_seed_tail]),
-			),
-		};
-
-		const regenerated_challenge = generateRegisterChallenge(
-			user.account.address,
-			version.toString(),
-			stored.nonce,
-		);
-
-		const regenerated_signature = await user.signMessage({
-			message: regenerated_challenge.challenge,
-		});
-
-		const regenerated = regenerateEncryptionKey(
-			regenerated_signature,
-			pin,
-			stored.salt_pin,
-			stored.salt_auth,
-			stored.salt_wrap,
-			stored.seed,
-			"test",
-		);
-
-		expect(regenerated.encryptionKey).to.equal(generated.encryptionKey);
+		await keyRegistry.write.registerKeygenData([salpin]);
 	});
 });
