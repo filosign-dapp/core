4207a3f7ba9b056c74642710ff223ceb88736ed2
Spandan Barve <spandan567@gmail.com>
2025-10-23 06:29:30 +0530

    keygen tests now pass, only one test is summarizing everyhhing thoug

--- packages/contracts/test/FSKeyRegistry.sol.test.ts
+++ packages/contracts/test/FSKeyRegistry.sol.test.ts
@@ -8,11 +8,11 @@ import {
 	signatures,
 	toBytes,
 	toHex,
+	walletKeyGen,
 } from "@filosign/crypto-utils/node";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
-import { expect } from "chai";
+import { expect, use } from "chai";
 import hre from "hardhat";
-import { describe, it } from "mocha";
 import {
 	concatBytes,
 	concatHex,
@@ -40,7 +40,33 @@ describe("FSKeyRegistry", () => {
 	it("stores relevant information for the user to be able to regenerate encryption keys", async () => {
 		const { keyRegistry, user, admin } = await loadFixture(setupFixture);
 		const pin = "1234";
+		const { saltPin, saltSeed, saltChallenge, commitmentKem, commitmentSig } =
+			await walletKeyGen(user, { pin });
 
-		await keyRegistry.write.registerKeygenData([salpin]);
+		await keyRegistry.write.registerKeygenData(
+			[saltPin, saltSeed, saltChallenge, commitmentKem, commitmentSig],
+			{ account: user.account },
+		);
+
+		const storedData = await keyRegistry.read.keygenData([
+			user.account.address,
+		]);
+
+		expect(storedData[0]).to.equal(saltPin);
+		expect(storedData[1]).to.equal(saltSeed);
+		expect(storedData[2]).to.equal(saltChallenge);
+		expect(storedData[3]).to.equal(commitmentKem);
+		expect(storedData[4]).to.equal(commitmentSig);
+
+		const reconstructed = await walletKeyGen(user, {
+			pin,
+			salts: {
+				pin: storedData[0],
+				seed: storedData[1],
+				challenge: storedData[2],
+			},
+		});
+		expect(reconstructed.commitmentKem).to.equal(commitmentKem);
+		expect(reconstructed.commitmentSig).to.equal(commitmentSig);
 	});
 });
