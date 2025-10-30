1d12e940304a9dc075c677be56edd899e15990c8
Spandan Barve <spandan567@gmail.com>
2025-10-31 03:54:43 +0530

    refactor: enhance FilosignProvider to initialize and manage Dilithium library

--- packages/client/src/lib/context/filosign-provider.tsx
+++ packages/client/src/lib/context/filosign-provider.tsx
@@ -1,16 +1,60 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: <explanationn nahi haih bhai koi explanation, chutia libry hai>  */
 import { FilosignProvider as FilosignProviderBase } from "@filosign/react";
-import { usePrivy } from "@privy-io/react-auth";
+import { useEffect, useState } from "react";
 import { useWalletClient } from "wagmi";
 
 export function FilosignProvider({ children }: { children: React.ReactNode }) {
-	const { data: wallet } = useWalletClient();
+    const { data: wallet } = useWalletClient();
 
-	return (
-		<FilosignProviderBase
-			apiBaseUrl={process.env.BUN_PUBLIC_PLATFORM_URL!}
-			wallet={wallet}
-		>
-			{children}
-		</FilosignProviderBase>
-	);
+    const [dilithium, setDilithium] = useState<any>(null);
+
+    useEffect(() => {
+        let mounted = true;
+
+        async function init() {
+            try {
+                // Mock chrome for the dilithium library
+                (globalThis as any).chrome = {
+                    runtime: {
+                        getURL: () => '/static/dilithium.wasm'
+                    }
+                };
+
+                const module = await import("dilithium-crystals-js/dist/dilithium.min.js");
+                let dil: any = null;
+                if (typeof module === "function") {
+                    dil = await (module as any)();
+                }
+                //  else if (module.default && typeof module.default === "function") {
+                //     dil = await module.default();
+                // }
+                else if (module.createDilithium && typeof module.createDilithium === "function") {
+                    dil = await module.createDilithium();
+                } else {
+                    dil = module;
+                }
+
+                if (!mounted) return;
+                setDilithium(dil);
+            } catch (err: any) {
+                console.error("Failed to init Dilithium:", err);
+            }
+        }
+
+        init();
+        return () => { mounted = false; };
+    }, []);
+
+
+    return (
+        <FilosignProviderBase
+            apiBaseUrl={process.env.BUN_PUBLIC_PLATFORM_URL!}
+            wasm={{
+                dilithium
+            }}
+            wallet={wallet}
+        >
+            {!!dilithium?.generateKeys && children}
+        </FilosignProviderBase>
+    );
 }
