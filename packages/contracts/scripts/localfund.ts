34caf678d71126e35cb0a6c745595e0f3a9e9f6c
Spandan Barve <spandan567@gmail.com>
2025-11-02 00:20:09 +0530

    improve variable naming and formatting in localfund script

--- packages/contracts/scripts/localfund.ts
+++ packages/contracts/scripts/localfund.ts
@@ -1,25 +1,39 @@
 import hre from "hardhat";
+import { getContracts } from "../exports";
 
 async function main() {
-	const chainId = hre.network.config.chainId;
-	if (chainId !== 31337) {
-		console.error("No ?");
-		process.exit(1);
-	}
+    const chainId = hre.network.config.chainId;
+    if (chainId !== 31337) {
+        console.error("No ?");
+        process.exit(1);
+    }
 
-	const [ay] = await hre.viem.getWalletClients();
+    const [u1, u2] = await hre.viem.getWalletClients();
 
-	for (const x of ["0xB8dd3786942057d4Bc78Fc894B80E8745151FE70"] as const) {
-		ay.sendTransaction({
-			to: x,
-			value: 1_000_000_000_000_000n,
-		});
-	}
+    for (const x of ["0xB8dd3786942057d4Bc78Fc894B80E8745151FE70"] as const) {
+        u1.sendTransaction({
+            to: x,
+            value: 1_000_000_000_000_000n,
+        });
+    }
+
+    const contracts1 = getContracts({
+        chainId: 31337,
+        client: u1
+    })
+    const contracts2 = getContracts({
+        chainId: 31337,
+        client: u2
+    })
+
+    await contracts1.FSManager.write.approveSender([u2.account.address])
+    await contracts2.FSManager.write.approveSender([u1.account.address])
 }
 
+
 main()
-	.then(() => console.log("Deployment script finished"))
-	.catch((e) => {
-		console.error(e);
-		process.exit(1);
-	});
+    .then(() => console.log("Deployment script finished"))
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
