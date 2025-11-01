a737a3f2ab0f1a2de403ee650ac766c0249affaf
Spandan Barve <spandan567@gmail.com>
2025-11-02 04:32:34 +0530

    json stringify stadard

--- packages/lib/react-sdk/utils/zod.ts
+++ packages/lib/react-sdk/utils/zod.ts
@@ -1,6 +1,6 @@
 import { isHex } from "viem";
 import z from "zod";
 
-export const zHexString = z.string().refine((val) => {
-	return isHex(val);
+export const zHexString = () => z.string().refine((val) => {
+    return isHex(val);
 });
