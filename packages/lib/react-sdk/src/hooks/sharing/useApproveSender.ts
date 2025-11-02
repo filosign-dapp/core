e561798c18354ba9aaaccf5db0ba5cce46091523
Spandan Barve <spandan567@gmail.com>
2025-11-03 01:40:07 +0530

    add useApproveSender hook for managing sender approvals

---
+++ packages/lib/react-sdk/src/hooks/sharing/useApproveSender.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Address } from "viem";
+import { useFilosignContext } from "../../context/FilosignProvider";
+
+export function useApproveSender() {
+    const { contracts, wallet, api } = useFilosignContext();
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async (sender: Address) => {
+            if (!contracts || !wallet) {
+                throw new Error("not conected iido");
+            }
+
+            const tx = await contracts.FSManager.write.approveSender([sender]);
+            const success = await api.rpc.tx(tx, {});
+
+            if (!success) {
+                throw new Error("Failed sverer to know you approve sender");
+            }
+
+            queryClient.refetchQueries({
+                queryKey: ["fsQ-is-approved", wallet?.account.address, sender],
+            });
+        },
+    });
+}
