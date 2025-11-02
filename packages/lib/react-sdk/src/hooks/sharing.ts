14f26321b8ced66efeb845e01aa70843d7925184
Spandan Barve <spandan567@gmail.com>
2025-11-03 01:41:04 +0530

    refactor: replace sharingCapability hook with sharing hooks for improved organization

---
+++ packages/lib/react-sdk/src/hooks/sharing.ts
@@ -0,0 +1,8 @@
+export { useApproveSender } from "./sharing/useApproveSender";
+export { useCancelRequest } from "./sharing/useCancelRequest";
+export { useCanReceiveFrom } from "./sharing/useCanReceiveFrom";
+export { useCanSendTo } from "./sharing/useCanSendTo";
+export { useReceivedRequests } from "./sharing/useReceivedRequests";
+export { useRejectRequest } from "./sharing/useRejectRequest";
+export { useRequestApproval } from "./sharing/useRequestApproval";
+export { useRevokeSender } from "./sharing/useRevokeSender";
