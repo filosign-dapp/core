02694b47367a737e87223667359756d4f68128df
Spandan Barve <spandan567@gmail.com>
2025-11-02 06:17:35 +0530

    refactor: update nonce generation and signature verification logic

--- packages/server/api/routes/auth/index.ts
+++ packages/server/api/routes/auth/index.ts
@@ -1,9 +1,9 @@
+import { signatures, toBytes } from "@filosign/crypto-utils/node"
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { type Address, type Hash, isAddress, isHex, keccak256 } from "viem";
+import { type Address, type Hash, isAddress, isHex, keccak256, numberToHex } from "viem";
 import { MINUTE } from "../../../constants";
 import db from "../../../lib/db";
-import { p256VerifyWithXOnly } from "../../../lib/utils/crypto";
 import { issueJwtToken } from "../../../lib/utils/jwt";
 import { respond } from "../../../lib/utils/respond";
 
@@ -12,70 +12,65 @@ const { users } = db.schema;
 
 export default new Hono()
 
-	.get("/nonce", async (ctx) => {
-		const wallet = ctx.req.query("address");
-		if (!wallet || !isAddress(wallet)) {
-			return respond.err(ctx, "Missing wallet address", 400);
-		}
+    .get("/nonce", async (ctx) => {
+        const wallet = ctx.req.query("address");
+        if (!wallet || !isAddress(wallet)) {
+            return respond.err(ctx, "Missing wallet address", 400);
+        }
 
-		const nonce = keccak256(Uint8Array.from(Bun.randomUUIDv7()));
-		nonces[wallet] = { nonce, validTill: Date.now() + 1 * MINUTE };
+        const nonce = keccak256(numberToHex(Date.now() + Math.random() * 1e10));
+        nonces[wallet] = { nonce, validTill: Date.now() + 5 * MINUTE };
 
-		return respond.ok(ctx, { nonce }, "nonce generated", 200);
-	})
+        return respond.ok(ctx, { nonce }, "nonce generated", 200);
+    })
 
-	.get("/verify", async (ctx) => {
-		const address = ctx.req.query("address");
-		const pubKey = ctx.req.query("pub_key");
-		const signature = ctx.req.query("signature");
+    .get("/verify", async (ctx) => {
+        const address = ctx.req.query("address");
+        const signature = ctx.req.query("signature");
 
-		if (!address || !isAddress(address)) {
-			return respond.err(ctx, "Missing or invalid address", 400);
-		}
+        if (!address || !isAddress(address)) {
+            return respond.err(ctx, "Missing or invalid address", 400);
+        }
+        if (!signature) {
+            return respond.err(ctx, "Missing signature", 400);
+        }
+        if (!isHex(signature)) {
+            return respond.err(ctx, "Invalid signature format", 400);
+        }
 
-		if (!pubKey || !isHex(pubKey)) {
-			return respond.err(ctx, "Missing or invalid public key", 400);
-		}
+        const msgData = nonces[address];
+        delete nonces[address];
 
-		console.log("signature", signature);
+        if (!msgData || msgData.validTill < Date.now()) {
+            return respond.err(ctx, "Message expired or not found", 400);
+        }
 
-		if (!signature) {
-			return respond.err(ctx, "Missing signature", 400);
-		}
+        const [userRecord] = await db
+            .select({
 
-		if (!isHex(signature)) {
-			return respond.err(ctx, "Invalid signature format", 400);
-		}
+                signaturePublicKey: users.signaturePublicKey
+            })
+            .from(users)
+            .where(eq(users.walletAddress, address))
+            .limit(1)
 
-		const msgData = nonces[address];
-		delete nonces[address];
+        if (!userRecord) {
+            return respond.err(ctx, "You are not registered", 401);
+        }
 
-		if (!msgData || msgData.validTill < Date.now()) {
-			return respond.err(ctx, "Message expired or not found", 400);
-		}
+        const { nonce } = msgData;
 
-		const { nonce } = msgData;
+        const valid = await signatures.verify({
+            dl: await signatures.dilithiumInstance(),
+            message: toBytes(nonce),
+            signature: toBytes(signature),
+            publicKey: toBytes(userRecord.signaturePublicKey)
+        })
 
-		console.log(signature, nonce, pubKey);
-		const valid = p256VerifyWithXOnly(signature, nonce, pubKey);
-		console.log("valid", valid);
+        if (!valid) {
+            return respond.err(ctx, "Invalid signature", 400);
+        }
 
-		if (!valid) {
-			return respond.err(ctx, "Invalid signature", 400);
-		}
-
-		console.log("pubKey", pubKey);
-
-		const user = db
-			.select()
-			.from(users)
-			.where(eq(users.encryptionPublicKey, pubKey))
-			.get();
-
-		if (!user) {
-			return respond.err(ctx, "User not found", 404);
-		}
-
-		const token = issueJwtToken(user.walletAddress);
-		return respond.ok(ctx, { valid, token }, "Signature verified", 200);
-	});
+        const token = issueJwtToken(address);
+        return respond.ok(ctx, { valid, token }, "Signature verified", 200);
+    });
--- packages/server/api/routes/sharing/index.ts
+++ packages/server/api/routes/sharing/index.ts
@@ -1,179 +1,163 @@
-// import { and, desc, eq, sql } from "drizzle-orm";
-// import { Hono } from "hono";
-// import {
-// 	encodePacked,
-// 	getAddress,
-// 	isAddress,
-// 	keccak256,
-// 	verifyMessage,
-// } from "viem";
-// import db from "../../../lib/db";
-// import { enqueueJob } from "../../../lib/jobrunner/scheduler";
-// import { respond } from "../../../lib/utils/respond";
-// import { authenticated } from "../../middleware/auth";
+export default new Hono()
+    .post("/request", async (ctx) => {
+        const { senderWallet, recipientWallet, message, signature } =
+            await ctx.req.json();
 
-// const { shareRequests, shareApprovals } = db.schema;
+        if (!recipientWallet || !isAddress(recipientWallet)) {
+            return respond.err(ctx, "Invalid recipientWallet", 400);
+        }
 
-// export default new Hono()
-// 	.post("/request", authenticated, async (ctx) => {
-// 		const { senderWallet, recipientWallet, message, signature } =
-// 			await ctx.req.json();
+        const recipient = getAddress(recipientWallet);
+        const sender = getAddress(senderWallet);
 
-// 		if (!recipientWallet || !isAddress(recipientWallet)) {
-// 			return respond.err(ctx, "Invalid recipientWallet", 400);
-// 		}
+        if (recipient === sender) {
+            return respond.err(ctx, "Don't ask yourself for permission", 400);
+        }
 
-// 		const recipient = getAddress(recipientWallet);
-// 		const sender = getAddress(senderWallet);
+        const newRequest = db
+            .insert(shareRequests)
+            .values({
+                senderWallet: wallet,
+                recipientWallet: recipient,
+                message: message.toString().slice(0, 255),
+                metadata: metadata,
+            })
+            .returning()
+            .get();
 
-// 		if (recipient === sender) {
-// 			return respond.err(ctx, "Don't ask yourself for permission", 400);
-// 		}
+        return respond.ok(ctx, newRequest, "Share request created", 201);
+    })
+    // .get("/received", authenticated, async (ctx) => {
+    // 	const rows = db
+    // 		.select()
+    // 		.from(shareRequests)
+    // 		.where(eq(shareRequests.recipientWallet, ctx.var.userWallet))
+    // 		.orderBy(shareRequests.createdAt)
+    // 		.all();
 
-// 		const newRequest = db
-// 			.insert(shareRequests)
-// 			.values({
-// 				senderWallet: wallet,
-// 				recipientWallet: recipient,
-// 				message: message.toString().slice(0, 255),
-// 				metadata: metadata,
-// 			})
-// 			.returning()
-// 			.get();
+    // 	if (rows.length === 0) {
+    // 		return respond.ok(ctx, { requests: [] }, "No received requests", 200);
+    // 	}
 
-// 		return respond.ok(ctx, newRequest, "Share request created", 201);
-// 	})
-// 	// .get("/received", authenticated, async (ctx) => {
-// 	// 	const rows = db
-// 	// 		.select()
-// 	// 		.from(shareRequests)
-// 	// 		.where(eq(shareRequests.recipientWallet, ctx.var.userWallet))
-// 	// 		.orderBy(shareRequests.createdAt)
-// 	// 		.all();
+    // 	// if status is not PENDING, remove from rows
+    // 	const filteredRows = rows.filter((row) => row.status === "PENDING");
 
-// 	// 	if (rows.length === 0) {
-// 	// 		return respond.ok(ctx, { requests: [] }, "No received requests", 200);
-// 	// 	}
+    // 	return respond.ok(
+    // 		ctx,
+    // 		{ requests: filteredRows },
+    // 		"Received requests fetched",
+    // 		200,
+    // 	);
+    // })
+    // .get("/sent", authenticated, async (ctx) => {
+    // 	const rows = db
+    // 		.select()
+    // 		.from(shareRequests)
+    // 		.where(eq(shareRequests.senderWallet, ctx.var.userWallet))
+    // 		.orderBy(shareRequests.createdAt)
+    // 		.all();
 
-// 	// 	// if status is not PENDING, remove from rows
-// 	// 	const filteredRows = rows.filter((row) => row.status === "PENDING");
+    // 	return respond.ok(ctx, { requests: rows }, "Sent requests fetched", 200);
+    // })
+    .get("/can-send-to", authenticated, async (ctx) => {
+        const subq = db
+            .select({
+                recipientWallet: shareApprovals.recipientWallet,
+                maxCreatedAt: sql<number>`max(${shareApprovals.createdAt})`,
+            })
+            .from(shareApprovals)
+            .where(eq(shareApprovals.senderWallet, ctx.var.userWallet))
+            .groupBy(shareApprovals.recipientWallet)
+            .as("subq");
 
-// 	// 	return respond.ok(
-// 	// 		ctx,
-// 	// 		{ requests: filteredRows },
-// 	// 		"Received requests fetched",
-// 	// 		200,
-// 	// 	);
-// 	// })
-// 	// .get("/sent", authenticated, async (ctx) => {
-// 	// 	const rows = db
-// 	// 		.select()
-// 	// 		.from(shareRequests)
-// 	// 		.where(eq(shareRequests.senderWallet, ctx.var.userWallet))
-// 	// 		.orderBy(shareRequests.createdAt)
-// 	// 		.all();
+        const rows = db
+            .select({
+                walletAddress: shareApprovals.recipientWallet,
+                active: shareApprovals.active,
+                createdAt: shareApprovals.createdAt,
+            })
+            .from(shareApprovals)
+            .innerJoin(
+                subq,
+                and(
+                    eq(shareApprovals.recipientWallet, subq.recipientWallet),
+                    eq(shareApprovals.createdAt, subq.maxCreatedAt),
+                ),
+            )
+            .where(eq(shareApprovals.active, true))
+            .execute();
 
-// 	// 	return respond.ok(ctx, { requests: rows }, "Sent requests fetched", 200);
-// 	// })
-// 	.get("/can-send-to", authenticated, async (ctx) => {
-// 		const subq = db
-// 			.select({
-// 				recipientWallet: shareApprovals.recipientWallet,
-// 				maxCreatedAt: sql<number>`max(${shareApprovals.createdAt})`,
-// 			})
-// 			.from(shareApprovals)
-// 			.where(eq(shareApprovals.senderWallet, ctx.var.userWallet))
-// 			.groupBy(shareApprovals.recipientWallet)
-// 			.as("subq");
+        return respond.ok(
+            ctx,
+            { people: rows },
+            "People you can send requests to",
+            200,
+        );
+    })
+    .get("/can-receive-from", authenticated, async (ctx) => {
+        const rows = db
+            .select({
+                walletAddress: shareApprovals.senderWallet,
+                username: profiles.username,
+                displayName: profiles.displayName,
+                avatarUrl: profiles.avatarUrl,
+                active: shareApprovals.active,
+                createdAt: shareApprovals.createdAt,
+            })
+            .from(shareApprovals)
+            .leftJoin(
+                profiles,
+                eq(shareApprovals.senderWallet, profiles.walletAddress),
+            )
+            .where(
+                and(
+                    eq(shareApprovals.recipientWallet, ctx.var.userWallet),
+                    eq(shareApprovals.active, true),
+                ),
+            )
+            .orderBy(shareApprovals.createdAt)
+            .all();
 
-// 		const rows = db
-// 			.select({
-// 				walletAddress: shareApprovals.recipientWallet,
-// 				active: shareApprovals.active,
-// 				createdAt: shareApprovals.createdAt,
-// 			})
-// 			.from(shareApprovals)
-// 			.innerJoin(
-// 				subq,
-// 				and(
-// 					eq(shareApprovals.recipientWallet, subq.recipientWallet),
-// 					eq(shareApprovals.createdAt, subq.maxCreatedAt),
-// 				),
-// 			)
-// 			.where(eq(shareApprovals.active, true))
-// 			.execute();
+        return respond.ok(
+            ctx,
+            { people: rows },
+            "People who can send you requests",
+            200,
+        );
+    })
+    .delete("/:id/cancel", authenticated, async (ctx) => {
+        const { id } = ctx.req.param();
+        if (!id) return respond.err(ctx, "Missing id parameter", 400);
 
-// 		return respond.ok(
-// 			ctx,
-// 			{ people: rows },
-// 			"People you can send requests to",
-// 			200,
-// 		);
-// 	})
-// 	.get("/can-receive-from", authenticated, async (ctx) => {
-// 		const rows = db
-// 			.select({
-// 				walletAddress: shareApprovals.senderWallet,
-// 				username: profiles.username,
-// 				displayName: profiles.displayName,
-// 				avatarUrl: profiles.avatarUrl,
-// 				active: shareApprovals.active,
-// 				createdAt: shareApprovals.createdAt,
-// 			})
-// 			.from(shareApprovals)
-// 			.leftJoin(
-// 				profiles,
-// 				eq(shareApprovals.senderWallet, profiles.walletAddress),
-// 			)
-// 			.where(
-// 				and(
-// 					eq(shareApprovals.recipientWallet, ctx.var.userWallet),
-// 					eq(shareApprovals.active, true),
-// 				),
-// 			)
-// 			.orderBy(shareApprovals.createdAt)
-// 			.all();
+        const row = db
+            .select()
+            .from(shareRequests)
+            .where(eq(shareRequests.id, id))
+            .get();
+        if (!row) return respond.err(ctx, "Request not found", 404);
 
-// 		return respond.ok(
-// 			ctx,
-// 			{ people: rows },
-// 			"People who can send you requests",
-// 			200,
-// 		);
-// 	})
-// 	.delete("/:id/cancel", authenticated, async (ctx) => {
-// 		const { id } = ctx.req.param();
-// 		if (!id) return respond.err(ctx, "Missing id parameter", 400);
+        const sender = getAddress(row.senderWallet);
+        if (sender !== ctx.var.userWallet) {
+            return respond.err(ctx, "Only the sender may cancel this request", 403);
+        }
 
-// 		const row = db
-// 			.select()
-// 			.from(shareRequests)
-// 			.where(eq(shareRequests.id, id))
-// 			.get();
-// 		if (!row) return respond.err(ctx, "Request not found", 404);
+        if (row.status !== "PENDING") {
+            return respond.err(ctx, "Only pending requests can be cancelled", 409);
+        }
 
-// 		const sender = getAddress(row.senderWallet);
-// 		if (sender !== ctx.var.userWallet) {
-// 			return respond.err(ctx, "Only the sender may cancel this request", 403);
-// 		}
+        db.update(shareRequests)
+            .set({
+                status: "CANCELLED",
+            })
+            .where(eq(shareRequests.id, id))
+            .run();
 
-// 		if (row.status !== "PENDING") {
-// 			return respond.err(ctx, "Only pending requests can be cancelled", 409);
-// 		}
+        void enqueueJob({
+            type: "request:cancelled",
+            payload: {
+                requestId: id,
+            },
+        });
 
-// 		db.update(shareRequests)
-// 			.set({
-// 				status: "CANCELLED",
-// 			})
-// 			.where(eq(shareRequests.id, id))
-// 			.run();
-
-// 		void enqueueJob({
-// 			type: "request:cancelled",
-// 			payload: {
-// 				requestId: id,
-// 			},
-// 		});
-
-// 		return respond.ok(ctx, { canceled: id }, `Request ${id} canceled`, 200);
-// 	});
+        return respond.ok(ctx, { canceled: id }, `Request ${id} canceled`, 200);
+    });
