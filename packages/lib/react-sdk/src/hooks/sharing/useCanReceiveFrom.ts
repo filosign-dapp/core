30f0c2946936537c421c2aaeec6464878a410f03
Spandan Barve <spandan567@gmail.com>
2025-11-03 01:40:18 +0530

    add useCanReceiveFrom hook for checking sender approval status

---
+++ packages/lib/react-sdk/src/hooks/sharing/useCanReceiveFrom.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import type { Address } from "viem";
+import { useFilosignContext } from "../../context/FilosignProvider";
+
+export function useCanReceiveFrom(sender: Address) {
+    const { contracts, wallet } = useFilosignContext();
+
+    return useQuery({
+        queryKey: ["fsQ-is-approved", sender, wallet?.account.address],
+        queryFn: async () => {
+            if (!contracts || !wallet) return false;
+            const isApproved = await contracts.FSManager.read.approvedSenders([
+                wallet.account.address,
+                sender,
+            ]);
+            return isApproved;
+        },
+        enabled: !!wallet && !!contracts,
+    });
+}
