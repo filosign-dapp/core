aeea3c1a77ae3b379eb0e31473f82c4606979554
Spandan Barve <spandan567@gmail.com>
2025-11-03 03:29:16 +0530

    refactor: enhance runtime handling in FilosignProvider and improve useSentFiles hook structure

--- packages/lib/react-sdk/src/context/FilosignProvider.tsx
+++ packages/lib/react-sdk/src/context/FilosignProvider.tsx
@@ -11,6 +11,8 @@ import {
 } from "react";
 import type { Chain } from "viem";
 import type { UseWalletClientReturnType } from "wagmi";
+import { z } from "zod";
+import { zHexString } from "../../utils/zod";
 import ApiClient from "../ApiClient";
 import { MINUTE } from "../constants";
 
@@ -21,6 +23,7 @@ type FilosignContext = {
     api: ApiClient;
     wallet: Wallet;
     contracts: FilosignContracts | null;
+    runtime: Runtime;
     wasm: {
         // biome-ignore lint/suspicious/noExplicitAny: pata nahi bhai, maa chudaa rha tha
         dilithium: any;
@@ -32,6 +35,7 @@ const FilosignContext = createContext<FilosignContext>({
     api: {} as ApiClient,
     wallet: undefined,
     contracts: null,
+    runtime: {} as Runtime,
     wasm: {
         dilithium: undefined,
     },
@@ -54,16 +58,18 @@ export function FilosignProvider(props: FilosignConfig) {
     const api = useMemo(() => new ApiClient(apiBaseUrl), [apiBaseUrl]);
 
     const runtime = useQuery({
-        queryKey: [
-            "runtime",
-            apiBaseUrl
-        ],
+        queryKey: ["runtime", apiBaseUrl],
         queryFn: async () => {
             console.log("Fetching runtime from", `${apiBaseUrl}/runtime`);
-            const runtime = await fetch(`${apiBaseUrl}/runtime`).then((res) =>
-                res.json(),
+            const parsed = await api.rpc.getSafe(
+                {
+                    uptime: z.number(),
+                    chain: z.any(),
+                    serverAddressSynapse: zHexString(),
+                },
+                "/runtime",
             );
-            return runtime as { uptime: number; chain: Chain };
+            return parsed.data;
         },
         staleTime: 5 * MINUTE,
 
@@ -85,18 +91,31 @@ export function FilosignProvider(props: FilosignConfig) {
 
     const value: FilosignContext = useMemo(
         () => ({
-            ready: !!api,
+            ready: !!api && !!runtime.data,
             wallet: wallet,
             api: api as ApiClient,
             contracts,
             wasm: { dilithium: wasm.dilithium },
+            runtime: runtime.data || ({} as Runtime),
         }),
-        [api, wallet, contracts, wasm],
+        [api, wallet, contracts, wasm, runtime.data],
     );
 
-    return <FilosignContext.Provider value={value}>{children}</FilosignContext.Provider>;
+    if (!runtime.data) return <>loadnging</>;
+
+    return (
+        <FilosignContext.Provider value={value}>
+            {children}
+        </FilosignContext.Provider>
+    );
 }
 
 export function useFilosignContext() {
     return useContext(FilosignContext);
 }
+
+type Runtime = {
+    uptime: number;
+    chain: Chain;
+    serverAddressSynapse: string;
+};
--- packages/lib/react-sdk/src/hooks/files/useSentFiles.ts
+++ packages/lib/react-sdk/src/hooks/files/useSentFiles.ts
@@ -1,25 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import z from "zod";
+import { MINUTE } from "../../constants";
 import { useFilosignContext } from "../../context/FilosignProvider";
 
 export function useSentFiles() {
-    const { api } = useFilosignContext();
+	const { api } = useFilosignContext();
 
-    return useQuery({
-        queryKey: ["sent-files"],
-        queryFn: async () => {
-            const response = await api.rpc.getSafe(
-                {
-                    sentFiles: z.array(z.object({
-                        pieceCid: z.string(),
-                        sender: z.string(),
-                        status: z.string(),
-                    })),
-                },
-                "/files/sent"
-            );
+	return useQuery({
+		queryKey: ["sent-files"],
+		queryFn: async () => {
+			const response = await api.rpc.getSafe(
+				{
+					sentFiles: z.array(
+						z.object({
+							pieceCid: z.string(),
+							sender: z.string(),
+							status: z.string(),
+						}),
+					),
+				},
+				"/files/sent",
+			);
 
-            return response.data.sentFiles;
-        },
-    });
+			return response.data.sentFiles;
+		},
+		staleTime: 5 * MINUTE,
+	});
 }
