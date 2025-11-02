00fad4cbec15ca3b46402dfe26a79acf6bf27535
Spandan Barve <spandan567@gmail.com>
2025-11-03 03:47:19 +0530

    𓑁

--- packages/lib/react-sdk/src/hooks/sharing/useReceivableFrom.ts
+++ packages/lib/react-sdk/src/hooks/sharing/useReceivableFrom.ts
@@ -3,23 +3,25 @@ import { z } from "zod";
 import { useFilosignContext } from "../../context/FilosignProvider";
 
 export function useReceivableFrom() {
-    const { api } = useFilosignContext();
+	const { api } = useFilosignContext();
 
-    return useQuery({
-        queryKey: ["receivable-from"],
-        queryFn: async () => {
-            const response = await api.rpc.getSafe(
-                {
-                    approvals: z.array(z.object({
-                        senderWallet: z.string(),
-                        active: z.boolean(),
-                        createdAt: z.string(),
-                    })),
-                },
-                "/sharing/receivable-from"
-            );
-            return response.data.approvals;
-        },
-        enabled: !!api,
-    });
+	return useQuery({
+		queryKey: ["receivable-from"],
+		queryFn: async () => {
+			const response = await api.rpc.getSafe(
+				{
+					approvals: z.array(
+						z.object({
+							senderWallet: z.string(),
+							active: z.boolean(),
+							createdAt: z.string(),
+						}),
+					),
+				},
+				"/sharing/receivable-from",
+			);
+			return response.data.approvals;
+		},
+		enabled: !!api,
+	});
 }
--- packages/lib/react-sdk/src/hooks/sharing/useSendableTo.ts
+++ packages/lib/react-sdk/src/hooks/sharing/useSendableTo.ts
@@ -3,23 +3,25 @@ import { z } from "zod";
 import { useFilosignContext } from "../../context/FilosignProvider";
 
 export function useSendableTo() {
-    const { api } = useFilosignContext();
+	const { api } = useFilosignContext();
 
-    return useQuery({
-        queryKey: ["sendable-to"],
-        queryFn: async () => {
-            const response = await api.rpc.getSafe(
-                {
-                    approvals: z.array(z.object({
-                        recipientWallet: z.string(),
-                        active: z.boolean(),
-                        createdAt: z.string(),
-                    })),
-                },
-                "/sharing/sendable-to"
-            );
-            return response.data.approvals;
-        },
-        enabled: !!api,
-    });
+	return useQuery({
+		queryKey: ["sendable-to"],
+		queryFn: async () => {
+			const response = await api.rpc.getSafe(
+				{
+					approvals: z.array(
+						z.object({
+							recipientWallet: z.string(),
+							active: z.boolean(),
+							createdAt: z.string(),
+						}),
+					),
+				},
+				"/sharing/sendable-to",
+			);
+			return response.data.approvals;
+		},
+		enabled: !!api,
+	});
 }
