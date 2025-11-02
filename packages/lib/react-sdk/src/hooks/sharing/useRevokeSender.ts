29de2461f195406be3a3193e621589dd186588d8
Spandan Barve <spandan567@gmail.com>
2025-11-03 01:40:50 +0530

    add useRevokeSender hook for revoking sender permissions

---
+++ packages/lib/react-sdk/src/hooks/sharing/useRevokeSender.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Address } from "viem";
+import { useFilosignContext } from "../../context/FilosignProvider";
+
+export function useRevokeSender() {
+    const { contracts, wallet, api } = useFilosignContext();
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async (sender: Address) => {
+            if (!contracts || !wallet) {
+                throw new Error("not conected iido");
+            }
+
+            const tx = await contracts.FSManager.write.revokeSender([sender]);
+            const success = await api.rpc.tx(tx, {});
+
+            if (!success) {
+                throw new Error("Failed sverer to know you revoke sender");
+            }
+
+            queryClient.refetchQueries({
+                queryKey: ["fsQ-is-approved", wallet?.account.address, sender],
+            });
+        },
+    });
+}
