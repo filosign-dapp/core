7e9098ba1d765357d2d296a7aeaac70663ef16c4
Spandan Barve <spandan567@gmail.com>
2025-11-04 05:17:18 +0530

    ah

--- packages/server/lib/db/schema/file.ts
+++ packages/server/lib/db/schema/file.ts
@@ -3,58 +3,58 @@ import { tBytes32, tEvmAddress, tHex, timestamps } from "../helpers";
 import { users } from "./user";
 
 export const files = t.pgTable(
-    "files",
-    {
-        pieceCid: t.text().notNull().primaryKey(),
-        sender: tEvmAddress()
-            .notNull()
-            .references(() => users.walletAddress),
+	"files",
+	{
+		pieceCid: t.text().notNull().primaryKey(),
+		sender: tEvmAddress()
+			.notNull()
+			.references(() => users.walletAddress),
 
-        senderEncryptedEncryptionKey: tHex().notNull(),
-        senderKemCiphertext: tHex().notNull(),
+		senderEncryptedEncryptionKey: tHex().notNull(),
+		senderKemCiphertext: tHex().notNull(),
 
-        status: t.text({ enum: ["s3", "foc", "unpaid_for", "invalid"] }).notNull(),
-        onchainTxHash: tBytes32().unique().notNull(),
+		status: t.text({ enum: ["s3", "foc", "unpaid_for", "invalid"] }).notNull(),
+		onchainTxHash: tBytes32().unique().notNull(),
 
-        ...timestamps,
-    },
-    (table) => [t.index("idx_files_owner").on(table.sender)],
+		...timestamps,
+	},
+	(table) => [t.index("idx_files_owner").on(table.sender)],
 );
 
 export const fileRecipients = t.pgTable(
-    "file_recipients",
-    {
-        filePieceCid: t
-            .text()
-            .notNull()
-            .references(() => files.pieceCid, { onDelete: "cascade" }),
-        ack: tHex(),
-        recipientWallet: tEvmAddress().notNull(),
-        kemCiphertext: tHex().notNull(),
-        encryptedEncryptionKey: tHex().notNull(),
+	"file_recipients",
+	{
+		filePieceCid: t
+			.text()
+			.notNull()
+			.references(() => files.pieceCid, { onDelete: "cascade" }),
+		ack: tHex(),
+		recipientWallet: tEvmAddress().notNull(),
+		kemCiphertext: tHex().notNull(),
+		encryptedEncryptionKey: tHex().notNull(),
 
-        ...timestamps,
-    },
-    // (table) => [t.index("idx_file_recipients_file").on(table.filePieceCid)],
+		...timestamps,
+	},
+	// (table) => [t.index("idx_file_recipients_file").on(table.filePieceCid)],
 );
 
 export const fileSignatures = t.pgTable(
-    "file_signatures",
-    {
-        filePieceCid: t
-            .text()
-            .notNull()
-            .references(() => files.pieceCid, { onDelete: "cascade" }),
-        signer: t.text().notNull(),
-        signatureVisualHash: tBytes32().notNull(),
-        evmSignature: tHex().notNull(),
-        dl3Signature: tHex().notNull(),
-        timestamp: t.integer().notNull(),
-        onchainTxHash: t.text(),
+	"file_signatures",
+	{
+		filePieceCid: t
+			.text()
+			.notNull()
+			.references(() => files.pieceCid, { onDelete: "cascade" }),
+		signer: t.text().notNull(),
+		signatureVisualHash: tBytes32().notNull(),
+		evmSignature: tHex().notNull(),
+		dl3Signature: tHex().notNull(),
+		timestamp: t.integer().notNull(),
+		onchainTxHash: t.text(),
 
-        ...timestamps,
-    },
-    (table) => [t.index("idx_signatures_file").on(table.filePieceCid)],
+		...timestamps,
+	},
+	(table) => [t.index("idx_signatures_file").on(table.filePieceCid)],
 );
 
 // export const fileKeys = t.sqliteTable(
