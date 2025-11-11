ba1e07183380f86648fd52fbb282b64f08bd36d8
Spandan Barve <spandan567@gmail.com>
2025-11-10 21:49:54 -0300

    separate error file sfor FSFileREgistry

---
+++ packages/contracts/src/errors/EFSFileRegistry.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+error OnlyServer();
+error SignatureExpired();
+error SenderNotRegistered();
+error SignerNotApproved(address signer, address sender);
+error BadSignersLength();
+error DuplicateSigner(address signer);
+error ZeroSigner();
+error UnsortedSigners();
+error FileAlreadyRegistered();
+error FileNotRegistered();
+error InvalidSigner();
+error InvalidSender();
+error AlreadySigned();
+error InvalidSignature();
