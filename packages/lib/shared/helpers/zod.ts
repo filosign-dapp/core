922dc9ebb28746cea4607dae8e94def25535ee21
Spandan Barve <spandan567@gmail.com>
2025-11-11 15:41:56 -0300

    zod zFileData

--- packages/lib/shared/helpers/zod.ts
+++ packages/lib/shared/helpers/zod.ts
@@ -36,3 +36,19 @@ export const zJsonString = () =>
 		},
 		{ error: "Invalid JSON string" },
 	);
+
+export const zFileData = () =>
+	z.object({
+		bytesB64: z.string(),
+		sender: zEvmAddress(),
+		timestamp: z.number(),
+		signers: z.array(
+			z.object({
+				address: zEvmAddress(),
+				signaturePosition: zHexString(),
+			}),
+		),
+		metadata: z.object({
+			name: z.string(),
+		}),
+	});
