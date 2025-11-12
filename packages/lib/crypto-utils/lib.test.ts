83cf5eca1a308d474959a9edcf7c5dff8a0a6134
Spandan Barve <spandan567@gmail.com>
2025-11-12 12:42:44 -0300

    crypto utils tests were outtoof date

--- packages/lib/crypto-utils/lib.test.ts
+++ packages/lib/crypto-utils/lib.test.ts
@@ -1,123 +1,128 @@
 import { describe, expect, it } from "bun:test";
-import cryptoUtils from "./src/lib-node";
+import cryptoUtils, { randomBytes } from "./src/lib-node";
 
-const { KEM, encryption, hash, signatures, utils } = cryptoUtils;
+const { KEM, encryption, signatures } = cryptoUtils;
+
+const dl = await signatures.dilithiumInstance();
 
 describe("KEM (Kyber1024)", async () => {
-	it("should generate keypair determistically by input seed", async () => {
-		const seed = utils.randomBytes(64);
-		const { privateKey: pvt1, publicKey: pub1 } = await KEM.keyGen({ seed });
-		const { privateKey: pvt2, publicKey: pub2 } = await KEM.keyGen({ seed });
-		expect(pvt1).toEqual(pvt2);
-		expect(pub1).toEqual(pub2);
+    it("should generate keypair determistically by input seed", async () => {
+        const seed = randomBytes(64);
+        const { privateKey: pvt1, publicKey: pub1 } = await KEM.keyGen({ seed });
+        const { privateKey: pvt2, publicKey: pub2 } = await KEM.keyGen({ seed });
+        expect(pvt1).toEqual(pvt2);
+        expect(pub1).toEqual(pub2);
 
-		const seed2 = utils.randomBytes(64);
-		const { privateKey: pvt3, publicKey: pub3 } = await KEM.keyGen({
-			seed: seed2,
-		});
-		expect(pvt1).not.toEqual(pvt3);
-		expect(pub1).not.toEqual(pub3);
-	});
+        const seed2 = randomBytes(64);
+        const { privateKey: pvt3, publicKey: pub3 } = await KEM.keyGen({
+            seed: seed2,
+        });
+        expect(pvt1).not.toEqual(pvt3);
+        expect(pub1).not.toEqual(pub3);
+    });
 
-	it("should be able to genarate, encapsulate and decapsulate shared secret among two parteis", async () => {
-		const receiver = await KEM.keyGen({ seed: utils.randomBytes(64) });
+    it("should be able to genarate, encapsulate and decapsulate shared secret among two parteis", async () => {
+        const receiver = await KEM.keyGen({ seed: randomBytes(64) });
 
-		const { ciphertext, sharedSecret: ssA } = await KEM.encapsulate({
-			publicKeyOther: receiver.publicKey,
-		});
-		const { sharedSecret: ssE } = await KEM.decapsulate({
-			ciphertext,
-			privateKeySelf: receiver.privateKey,
-		});
+        const { ciphertext, sharedSecret: ssA } = await KEM.encapsulate({
+            publicKeyOther: receiver.publicKey,
+        });
+        const { sharedSecret: ssE } = await KEM.decapsulate({
+            ciphertext,
+            privateKeySelf: receiver.privateKey,
+        });
 
-		expect(ssA).toEqual(ssE);
+        expect(ssA).toEqual(ssE);
 
-		console.log("KEM public key size: ", receiver.publicKey.length);
-	});
+        console.log("KEM public key size: ", receiver.publicKey.length);
+    });
 });
 
 describe("Signatures (Dilithium)", async () => {
-	it("should geenerate keypair determistically by input seed", async () => {
-		const seed = utils.randomBytes(64);
-		const { privateKey: pvt1, publicKey: pub1 } = await signatures.keyGen({
-			seed,
-		});
-		const { privateKey: pvt2, publicKey: pub2 } = await signatures.keyGen({
-			seed,
-		});
-		expect(pvt1).toEqual(pvt2);
-		expect(pub1).toEqual(pub2);
+    it("should geenerate keypair determistically by input seed", async () => {
+        const seed = randomBytes(64);
+        const { privateKey: pvt1, publicKey: pub1 } = await signatures.keyGen({
+            seed, dl
+        });
+        const { privateKey: pvt2, publicKey: pub2 } = await signatures.keyGen({
+            seed, dl
+        });
+        expect(pvt1).toEqual(pvt2);
+        expect(pub1).toEqual(pub2);
 
-		const seed2 = utils.randomBytes(64);
-		const { privateKey: pvt3, publicKey: pub3 } = await signatures.keyGen({
-			seed: seed2,
-		});
-		expect(pvt1).not.toEqual(pvt3);
-		expect(pub1).not.toEqual(pub3);
-	});
+        const seed2 = randomBytes(64);
+        const { privateKey: pvt3, publicKey: pub3 } = await signatures.keyGen({
+            seed: seed2, dl
+        });
+        expect(pvt1).not.toEqual(pvt3);
+        expect(pub1).not.toEqual(pub3);
+    });
 
-	it("should be able to sign and verify message", async () => {
-		const signer = await signatures.keyGen({
-			seed: utils.randomBytes(64),
-		});
-		const message = utils.randomBytes(2056);
+    it("should be able to sign and verify message", async () => {
+        const signer = await signatures.keyGen({
+            seed: randomBytes(64), dl
+        });
+        const message = randomBytes(2056);
 
-		const signature = await signatures.sign({
-			message,
-			privateKey: signer.privateKey,
-		});
-		const isValid = await signatures.verify({
-			message,
-			signature,
-			publicKey: signer.publicKey,
-		});
-		expect(isValid).toBe(true);
+        const signature = await signatures.sign({
+            dl,
+            message,
+            privateKey: signer.privateKey,
+        });
+        const isValid = await signatures.verify({
+            dl,
+            message,
+            signature,
+            publicKey: signer.publicKey,
+        });
+        expect(isValid).toBe(true);
 
-		message.fill(0xff, 0, 10); // tamper the message
-		const isValidFake = await signatures.verify({
-			message,
-			signature,
-			publicKey: signer.publicKey,
-		});
-		expect(isValidFake).toBe(false);
+        message.fill(0xff, 0, 10); // tamper the message
+        const isValidFake = await signatures.verify({
+            dl,
+            message,
+            signature,
+            publicKey: signer.publicKey,
+        });
+        expect(isValidFake).toBe(false);
 
-		console.log("DL3 Public key size: ", signer.publicKey.length);
-		console.log("Signature size: ", signature.length);
-	});
+        console.log("DL3 Public key size: ", signer.publicKey.length);
+        console.log("Signature size: ", signature.length);
+    });
 });
 
 describe("Encryption (AES-GCM)", async () => {
-	it("entire encryption workflow with KEM is tested and it works", async () => {
-		const receiver = await KEM.keyGen({ seed: utils.randomBytes(64) });
+    it("entire encryption workflow with KEM is tested and it works", async () => {
+        const receiver = await KEM.keyGen({ seed: randomBytes(64) });
 
-		//sender
-		const { ciphertext: kemCiphertext, sharedSecret: ssA } =
-			await KEM.encapsulate({
-				publicKeyOther: receiver.publicKey,
-			});
+        //sender
+        const { ciphertext: kemCiphertext, sharedSecret: ssA } =
+            await KEM.encapsulate({
+                publicKeyOther: receiver.publicKey,
+            });
 
-		const message = utils.randomBytes(16_777_216);
+        const message = randomBytes(16_777_216);
 
-		const encryptedMessage = await encryption.encrypt({
-			message,
-			sharedSecret: ssA,
-			info: "encryption info",
-		});
+        const encryptedMessage = await encryption.encrypt({
+            message,
+            secretKey: ssA,
+            info: "encryption info",
+        });
 
-		const sent = { kemCiphertext, encryptedMessage };
+        const sent = { kemCiphertext, encryptedMessage };
 
-		//receiver
-		const { sharedSecret: ssE } = await KEM.decapsulate({
-			ciphertext: sent.kemCiphertext,
-			privateKeySelf: receiver.privateKey,
-		});
+        //receiver
+        const { sharedSecret: ssE } = await KEM.decapsulate({
+            ciphertext: sent.kemCiphertext,
+            privateKeySelf: receiver.privateKey,
+        });
 
-		const decryptedMessage = await encryption.decrypt({
-			ciphertext: sent.encryptedMessage,
-			sharedSecret: ssE,
-			info: "encryption info",
-		});
+        const decryptedMessage = await encryption.decrypt({
+            ciphertext: sent.encryptedMessage,
+            secretKey: ssE,
+            info: "encryption info",
+        });
 
-		expect(decryptedMessage).toEqual(message);
-	});
+        expect(decryptedMessage).toEqual(message);
+    });
 });
