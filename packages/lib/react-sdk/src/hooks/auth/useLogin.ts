59799040b5a79de51c58d1d16d3be64e47a46350
Spandan Barve <spandan567@gmail.com>
2025-11-13 10:06:08 -0300

    improve useLogin to avoid pr

--- packages/lib/react-sdk/src/hooks/auth/useLogin.ts
+++ packages/lib/react-sdk/src/hooks/auth/useLogin.ts
@@ -6,84 +6,85 @@ import { useIsLoggedIn } from "./useIsLoggedIn";
 import { useIsRegistered } from "./useIsRegistered";
 
 export function useLogin() {
-	const { api, contracts, wallet, wasm } = useFilosignContext();
-	const queryClient = useQueryClient();
+    const { api, contracts, wallet, wasm } = useFilosignContext();
+    const queryClient = useQueryClient();
 
-	const { data: isRegistered } = useIsRegistered();
-	const { data: isLoggedIn } = useIsLoggedIn();
+    const { data: isRegistered } = useIsRegistered();
+    const { data: isLoggedIn } = useIsLoggedIn();
 
-	return useMutation({
-		mutationFn: async (params: { pin: string }) => {
-			if (isLoggedIn) return true;
+    return useMutation({
+        mutationFn: async (params: { pin: string }) => {
+            if (isLoggedIn) return true;
 
-			const { pin } = params;
+            const { pin } = params;
 
-			if (!contracts || !wallet || !wasm.dilithium) {
-				throw new Error("unreachable");
-			}
+            if (!contracts || !wallet || !wasm.dilithium) {
+                throw new Error("unreachable");
+            }
 
-			const keyStore = idb({
-				db: wallet.account.address,
-				store: "fs-keystore",
-			});
+            const keyStore = idb({
+                db: wallet.account.address,
+                store: "fs-keystore",
+            });
 
-			if (!isRegistered) {
-				const keygenData = await walletKeyGen(wallet, {
-					pin,
-					dl: wasm.dilithium,
-				});
+            if (!isRegistered) {
+                const keygenData = await walletKeyGen(wallet, {
+                    pin,
+                    dl: wasm.dilithium,
+                });
 
-				const tx = await contracts.FSKeyRegistry.write.registerKeygenData([
-					keygenData.saltPin,
-					keygenData.saltSeed,
-					keygenData.saltChallenge,
-					keygenData.commitmentKem,
-					keygenData.commitmentSig,
-				]);
+                const tx = await contracts.FSKeyRegistry.write.registerKeygenData([
+                    keygenData.saltPin,
+                    keygenData.saltSeed,
+                    keygenData.saltChallenge,
+                    keygenData.commitmentKem,
+                    keygenData.commitmentSig,
+                ]);
 
-				const success = await api.rpc.tx(tx, {
-					encryptionPublicKey: toHex(keygenData.kemKeypair.publicKey),
-					signaturePublicKey: toHex(keygenData.sigKeypair.publicKey),
-				});
+                const success = await api.rpc.tx(tx, {
+                    encryptionPublicKey: toHex(keygenData.kemKeypair.publicKey),
+                    signaturePublicKey: toHex(keygenData.sigKeypair.publicKey),
+                });
 
-				if (!success) {
-					throw new Error("Failed to register keygen data");
-				}
+                if (!success) {
+                    throw new Error("Failed to register keygen data");
+                }
 
-				await keyStore.secret.put("key-seed", new Uint8Array(keygenData.seed));
-			} else {
-				const [saltPin, saltSeed, saltChallenge, commitmentKem, commitmentSig] =
-					await contracts.FSKeyRegistry.read.keygenData([
-						wallet.account.address,
-					]);
+                await keyStore.secret.put("key-seed", new Uint8Array(keygenData.seed));
+            } else {
+                const [saltPin, saltSeed, saltChallenge, commitmentKem, commitmentSig] =
+                    await contracts.FSKeyRegistry.read.keygenData([
+                        wallet.account.address,
+                    ]);
 
-				const keygenData = await walletKeyGen(wallet, {
-					pin,
-					salts: {
-						challenge: saltChallenge,
-						seed: saltSeed,
-						pin: saltPin,
-					},
-					dl: wasm.dilithium,
-				});
+                const keygenData = await walletKeyGen(wallet, {
+                    pin,
+                    salts: {
+                        challenge: saltChallenge,
+                        seed: saltSeed,
+                        pin: saltPin,
+                    },
+                    dl: wasm.dilithium,
+                });
 
-				if (
-					commitmentKem !== keygenData.commitmentKem ||
-					commitmentSig !== keygenData.commitmentSig
-				) {
-					throw new Error("Invalid PIN");
-				}
+                if (
+                    commitmentKem !== keygenData.commitmentKem ||
+                    commitmentSig !== keygenData.commitmentSig
+                ) {
+                    throw new Error("Invalid PIN");
+                }
 
-				await keyStore.secret.put("key-seed", new Uint8Array(keygenData.seed));
-			}
+                await keyStore.secret.put("key-seed", new Uint8Array(keygenData.seed));
+            }
 
-			queryClient.refetchQueries({
-				queryKey: ["fsQ-is-logged-in", wallet?.account.address],
-			});
-			queryClient.refetchQueries({
-				queryKey: ["fsQ-is-registered", wallet?.account.address],
-			});
-			return true;
-		},
-	});
+            queryClient.refetchQueries({
+                queryKey: ["fsQ-is-registered", wallet?.account.address],
+            }).then(() => queryClient.refetchQueries({
+                queryKey: ["fsQ-stored-keygen-data", wallet?.account.address],
+            }).then(() => queryClient.refetchQueries({
+                queryKey: ["fsQ-is-logged-in", wallet?.account.address],
+            })));
+            return true;
+        },
+    });
 }
