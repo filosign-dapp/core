dd1325bf3e369d922de0fe9b267e546bf0aaae0b
Spandan Barve <spandan567@gmail.com>
2025-11-15 00:00:02 -0300

    add signature routes to router

--- packages/server/api/routes/users/index.ts
+++ packages/server/api/routes/users/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import profile from "./profile";
-// import signatures from "./signatures";
+import signatures from "./signatures";
 
-export default new Hono().route("/profile", profile);
-// .route("/signatures", signatures);
+export default new Hono()
+	.route("/profile", profile)
+	.route("/signatures", signatures);
